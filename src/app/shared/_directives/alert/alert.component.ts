33d573f9cc1625de4dd7045ad29617c1cd624b89
adrisons <adries.91@gmail.com>
2017-12-28 19:25:38 +0100

    BUBBLE-145::Notificaciones flotantes

--- src/app/shared/_directives/alert/alert.component.ts
+++ src/app/shared/_directives/alert/alert.component.ts
@@ -6,7 +6,8 @@ import { AlertService } from '../../_services/alert.service';
 @Component({
     moduleId: module.id,
     selector: 'app-alert',
-    templateUrl: 'alert.component.html'
+    templateUrl: 'alert.component.html',
+    styleUrls: ['./alert.component.scss']
 })
 
 export class AlertComponent implements OnInit {
